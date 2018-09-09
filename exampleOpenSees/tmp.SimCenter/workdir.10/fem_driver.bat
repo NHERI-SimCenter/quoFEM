9002167935b0ceb998e33a74f1843465383026a4
Unknown <npdhye@gmail.com>
2018-09-08 21:04:35 -0700

    Updates on post-processing sampling

---
+++ exampleOpenSees/tmp.SimCenter/workdir.10/fem_driver.bat
@@ -0,0 +1,4 @@
+python UserDefinedTransformation.py
+perl C:\Users\nikhil\Desktop\dakota-6.7-release-public-Windows.x86-UI\bin\dprepro params.in SimCenterParams.template SimCenterParamIN.ops
+ OpenSees SimCenterInput.ops >> ops.out
+python postprocess.py Node_3_Disp
