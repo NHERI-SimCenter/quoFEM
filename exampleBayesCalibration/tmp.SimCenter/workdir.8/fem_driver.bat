a48b3c345beacb5378649e32877feb62239e9043
Unknown <npdhye@gmail.com>
2018-09-16 21:40:58 -0700

    Complete postprocessing enhancements.

---
+++ exampleBayesCalibration/tmp.SimCenter/workdir.8/fem_driver.bat
@@ -0,0 +1,3 @@
+perl C:\Users\nikhil\Desktop\dakota-6.7-release-public-Windows.x86-UI\bin\dprepro params.in SimCenterParams.template SimCenterParamIN.ops
+ OpenSees SimCenterInput.ops >> ops.out
+python postprocess.py Roof_Disp_3 Roof_Disp_4
