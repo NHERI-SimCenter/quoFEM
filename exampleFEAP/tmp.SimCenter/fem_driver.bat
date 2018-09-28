ac891f48ec847d484791cfe0d1dd1ca4a93739d3
Charles Wang <c_w@berkeley.edu>
2018-09-27 18:04:37 -0700

    cleaning

--- exampleFEAP/tmp.SimCenter/fem_driver.bat
+++
@@ -1,3 +0,0 @@
-python UserDefinedTransformation.py
-perl C:\Users\nikhil\Desktop\dakota-6.7-release-public-Windows.x86-UI\bin\dprepro params.in TrussTemplate.txt SimCenterIn.txt --output-format='\%10.5f'
-echo y|
