d9d0e860e6e45e63024cd1bf81cdee764547a609
claudioperez <50180406+claudioperez@users.noreply.github.com>
2020-10-22 15:49:25 -0700

    cmp - Continue reorganizing examples.

---
+++ examples/quo-02/src/TrussParams.py
@@ -0,0 +1,6 @@
+# set some parameters
+
+E = 205
+P = 25
+Ao = 250
+Au = 500
---
+++ examples/quo-06/src/Rosen.py
@@ -0,0 +1,19 @@
+#!/usr/bin/python
+
+# written: adamzs 06/20
+# The global minimum is at (a, a^2)
+
+import sys
+
+from params import *
+
+def rosenbrock(x, y):
+	a = 1.
+	b = 100.
+	return (a - x)**2. + b*(y - x**2.)**2.
+
+if __name__ == "__main__":
+	with open('results.out', 'w') as f:
+		result = rosenbrock(X, Y)
+		f.write('{:.60g}'.format(result))
+
