b491fbc1792b095b8665e1cc46496d460290e866
fmckenna <fmckenna@berkeley.edu>
2021-03-19 20:14:43 -0700

    fmk - adding file for appveyor

---
+++ makeEXE.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+
+# build backend
+cd backend
+mkdir -p build
+cd build
+cmake -DCMAKE_BUILD_TYPE=Release ..
+make
+make install
+
+# build UI
+cd ..
+cd ..
+mkdir -p build
+cd build
+conan install .. --build missing
+qmake ../quoFEM.pro
+make
+
+
