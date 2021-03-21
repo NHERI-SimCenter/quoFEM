f0f030cf302fd7ca971b5acff6014f5ad42c81d5
fmckenna <fmckenna@berkeley.edu>
2021-03-20 23:30:19 -0700

    fmk - removing reference to bincrafters

--- conanfile.py
+++ conanfile.py
@@ -9,9 +9,9 @@ class quoFEM(ConanFile):
     description = "NHERI SimCenter R2D Tool"
     settings = "os", "compiler", "build_type", "arch"
     generators = "qmake"
-    requires = "jansson/2.11@bincrafters/stable", \
+    requires = "jansson/2.13.1", \
                "zlib/1.2.11", \
-               "libcurl/7.64.1"
+               "libcurl/7.72.0"
     build_policy = "missing"
 
     def configure(self):
