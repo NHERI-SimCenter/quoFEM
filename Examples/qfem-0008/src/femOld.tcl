15c0f7269f3be7b28cbd3147aecad3bc0274a458
bsaakash <11618528+bsaakash@users.noreply.github.com>
2021-05-18 16:44:13 -0400

    abs - updating examples to follow new calibration data workflow

--- Examples/qfem-0008/src/femOld.tcl
+++ Examples/qfem-0008/src/femOld.tcl
@@ -1,9 +1,19 @@
+
 # units kips, in
 
-# 
-# some parameters
-#
+set numMeasurements 5
+
+set measuredLambdas {1025.21 1138.11 1099.39 1002.41 1052.69}
+set sigmaLambda 54.203
+
+set measuredPhis {1.53 1.24 1.38 1.50 1.35}
+set sigmaPhi 0.0705
+
 set PI 3.14159
+
+# 
+# some paramaters
+#
 
 set m1 0.52
 set m2 0.26
@@ -13,11 +23,10 @@ set h  120.0
 set E 29000.0 
 
 set Ac 110.0 
-set Ib 10e+8
-set Ab 10e+8 
-
 pset Ic1 1190.0
 pset Ic2 1190.0
+set Ib 10e+8
+set Ab 10e+8 
 
 #
 # build the model
@@ -56,7 +65,15 @@ set eigenValues [eigen -fullGenLapack $numModes]
 set phi [expr [nodeEigenvector 5 1 1] / [nodeEigenvector 3 1 1]]
 set lambda [lindex $eigenValues 0]
 
+set error {}
+for {set m 0} {$m < $numMeasurements} {incr m 1} {
+    lappend error [expr ([lindex $measuredLambdas $m] - $lambda) / $sigmaLambda]
+}
+for {set m 0} {$m < $numMeasurements} {incr m 1} {
+    lappend error [expr ([lindex $measuredPhis $m] - $phi) / $sigmaPhi]
+}
+
 # create results file
 set outFile [open "results.out" w]
-puts $outFile "$lambda $phi"
+puts $outFile "$error"
 close $outFile
