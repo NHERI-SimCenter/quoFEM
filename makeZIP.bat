94ebe9e30a1781373e1c69b66cf185db7cc030c7
unknown <fmckenna@berkeley.edu>
2024-05-13 00:24:50 -0700

    fmk - updating makeZip

---
+++ makeZIP.bat
@@ -0,0 +1,41 @@
+
+REM ## make the exe
+
+cd build
+conan install .. --build missing
+qmake ..\quoFEM.pro
+nmake
+
+REM ## copy application into folder and winddeployqt it
+
+copy quoFEM.exe .\quoFEM_Windows_Download
+cd quoFEM_Windows_Download
+windeployqt quoFEM.exe
+
+REM ## copy examples
+
+rmdir /s /q .\Examples
+mkdir .\Examples
+xcopy /s /e /Q ..\..\Examples .\Examples
+
+REM ## delete applications folder and copy new stuff
+
+rmdir /s /q .\applications
+mkdir .\applications
+mkdir .\applications\performFEM
+mkdir .\applications\performUQ
+mkdir .\applications\Workflow
+mkdir .\applications\opensees
+mkdir .\applications\dakota
+mkdir .\applications\python
+mkdir .\applications\common
+xcopy /s /e ..\..\..\SimCenterBackendApplications\applications\performFEM  .\applications\performFEM
+xcopy /s /e ..\..\..\SimCenterBackendApplications\applications\performUQ  .\applications\performUQ
+xcopy /s /e ..\..\..\SimCenterBackendApplications\applications\Workflow  .\applications\Workflow
+xcopy /s /e ..\..\..\SimCenterBackendApplications\applications\OpenSees  .\applications\opensees
+xcopy /s /e ..\..\..\SimCenterBackendApplications\applications\dakota  .\applications\dakota
+xcopy /s /e /Q ..\..\..\SimCenterBackendApplications\applications\python  .\applications\python
+xcopy /s /e ..\..\..\SimCenterBackendApplications\applications\common  .\applications\common
+
+
+
