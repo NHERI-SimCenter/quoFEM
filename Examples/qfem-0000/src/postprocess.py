bf907eca349f1a38b001a317e3ed4f65280d4fb2
bsaakash <11618528+bsaakash@users.noreply.github.com>
2024-05-23 13:39:25 -0700

    abs - modifying discrete distribution and formatting postprocess script

--- Examples/qfem-0000/src/postprocess.py
+++ Examples/qfem-0000/src/postprocess.py
@@ -1,8 +1,9 @@
 from pathlib import Path
 
-def read_data_from_file(file_path: Path, results: list=[]):
+
+def read_data_from_file(file_path: Path, results: list = []):
     # Open the file
-    with open(file_path, 'r') as file:
+    with open(file_path, "r") as file:
         # Read the first two lines
         for line in file.readlines()[:2]:
             # Split the line into values and convert them to floats
@@ -10,9 +11,13 @@ def read_data_from_file(file_path: Path, results: list=[]):
             results.extend([float(value) for value in values])
     return results
 
+
 if __name__ == "__main__":
     # List of file paths to read data from
-    file_path_list = [Path("AIM.json0.max_abs_acceleration.response.11.out"), Path("AIM.json0.max_rel_disp.response.11.out")]
+    file_path_list = [
+        Path("AIM.json0.max_abs_acceleration.response.11.out"),
+        Path("AIM.json0.max_rel_disp.response.11.out"),
+    ]
     results = []
     for file_path in file_path_list:
         # Read data from each file and append it to the results list
@@ -21,4 +26,3 @@ if __name__ == "__main__":
     # Write the results to a file
     with open("results.out", "w") as file:
         file.write(" ".join([str(result) for result in results]))
-    
