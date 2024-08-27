5a1ab80a3a7e8a361d52ba7def3b681b48d4a795
Noam Elisha <noam.elisha@berkeley.edu>
2024-08-27 16:58:22 -0700

    update paths to run examples

--- run_examples.sh
+++ run_examples.sh
@@ -6,14 +6,14 @@ git clone --branch master --depth 1 https://github.com/NHERI-SimCenter/quoFEM.gi
 
 
 # Add the current dir in the example file
-sed -i "s|{Current_Dir}|$(pwd)|g" $PWD/quoFEM/Examples/E1BasicHAZUS/input.json
+sed -i "s|{Current_Dir}|$(pwd)|g" $PWD/quoFEM/Examples/qfem-001/src/input.json
 
 # Create the working directoy
 mkdir tmp.SimCenter
 
 # Copy over the input data
-cp -R $PWD/quoFEM/Examples/E1BasicHAZUS/input_data .
+cp -R $PWD/quoFEM/Examples/qfem-001/src/input_data .
 
 # Run the example in the backend
-python $PWD/SimCenterBackendApplications/applications/Workflow/qWHALE.py "runningLocal" $PWD/quoFEM/Examples/qfem-001/srcr/input.json $PWD/SimCenterBackendApplications/applications/Workflow/WorkflowApplications.json
+python $PWD/SimCenterBackendApplications/applications/Workflow/qWHALE.py "runningLocal" $PWD/quoFEM/Examples/qfem-001/src/input.json $PWD/SimCenterBackendApplications/applications/Workflow/WorkflowApplications.json
 
