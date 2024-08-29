ebba08f853ecfcac69de091718a8d4c8a9e36743
Noam Elisha <noam.elisha@berkeley.edu>
2024-08-28 17:12:48 -0700

    idk anymoe

--- run_examples.sh
+++ run_examples.sh
@@ -10,14 +10,14 @@ echo In folder $PWD
 ls
 
 # Add the current dir in the example file
-sed -i "s|{Current_Dir}|$(pwd)|g" $PWD/quoFEM/Examples/qfem-001/src/input.json
+sed -i "s|{Current_Dir}|$(pwd)|g" $PWD/Examples/qfem-001/src/input.json
 
 # Create the working directoy
 mkdir tmp.SimCenter
 
 # Copy over the input data
-cp -R $PWD/quoFEM/Examples/qfem-001/src/input_data .
+cp -R $PWD/Examples/qfem-001/src/input_data .
 
 # Run the example in the backend
-python $PWD/SimCenterBackendApplications/applications/Workflow/qWHALE.py "runningLocal" $PWD/quoFEM/Examples/qfem-001/src/input.json $PWD/SimCenterBackendApplications/applications/Workflow/WorkflowApplications.json
+python $PWD/SimCenterBackendApplications/applications/Workflow/qWHALE.py "runningLocal" $PWD/Examples/qfem-001/src/input.json $PWD/SimCenterBackendApplications/applications/Workflow/WorkflowApplications.json
 
