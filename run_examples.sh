84baaf6458cb99723d48b5aba0d8513ea90e912a
Noam Elisha <noam.elisha@berkeley.edu>
2024-09-03 14:08:50 -0700

    change order pipe

--- run_examples.sh
+++ run_examples.sh
@@ -12,8 +12,7 @@ ls
 
 sudo apt-get install jq
 
-jq '. + { "runDir": '$PWD' }' | "$jsonStr"
-
+cat $PWD/Examples/qfem-0001/src/input.json  | jq '. + { "runDir": '$PWD' }'
 # # Add the current dir in the example file
 # sed -i "s|{Current_Dir}|$(pwd)|g" $PWD/Examples/qfem-0001/src/input.json
 
