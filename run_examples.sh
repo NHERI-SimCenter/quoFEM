5c8e12e26123d984c5cc7bf0fb1b73aa934ddc0d
Noam Elisha <noam.elisha@berkeley.edu>
2024-09-03 14:12:37 -0700

    added quotes

--- run_examples.sh
+++ run_examples.sh
@@ -12,7 +12,7 @@ ls
 
 sudo apt-get install jq
 
-cat $PWD/Examples/qfem-0001/src/input.json  | jq '. + { "runDir": '$PWD' }'
+cat $PWD/Examples/qfem-0001/src/input.json  | jq '. + { "runDir": '"$PWD"' }'
 # # Add the current dir in the example file
 # sed -i "s|{Current_Dir}|$(pwd)|g" $PWD/Examples/qfem-0001/src/input.json
 
